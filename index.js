783387073df669150b4fac1eeb6fd1729625c064
GLD5000 <garethlouisdevlin@gmail.com>
2023-06-29 00:02:57 +0100

    Update name

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -10,7 +11,26 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+const upload = multer({ dest: 'uploads/' });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // Access the file metadata through req.file
+
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  const { originalname, mimetype, size } = req.file;
+
+  // Create a response object containing the file metadata
+  const metadata = {
+    name: originalname,
+    type: mimetype,
+    size: size
+  };
+
+  // Send the metadata as the response
+  res.json(metadata);
+});
 
 
 
